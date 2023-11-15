b830586bffbb452f0d23552125e42fe3eecdfce7
bjgodfj77 <150914353+bjgodfj77@users.noreply.github.com>
2023-11-15 18:39:46 +0000

    Added the cart functionality of the ecommerce section and a back button.

--- e-commerce.js
+++ e-commerce.js
@@ -1,13 +1,16 @@
 // e-commerce.js
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Sample product data
-    const products = [
-      { id: 1, name: 'Product 1', price: 19.99 },
-      { id: 2, name: 'Product 2', price: 29.99 },
-      { id: 3, name: 'Product 3', price: 39.99 },
-    ];
+// Sample product data
+const products = [
+    { id: 1, name: 'Product 1', price: 19.99 },
+    { id: 2, name: 'Product 2', price: 29.99 },
+    { id: 3, name: 'Product 3', price: 39.99 },
+  ];
   
+  // Cart to store added products
+  const cart = [];
+  
+  document.addEventListener('DOMContentLoaded', function () {
     // Render products
     const productList = document.getElementById('product-list');
   
@@ -25,11 +28,30 @@ document.addEventListener('DOMContentLoaded', function () {
   
   // Function to simulate adding a product to the cart
   function addToCart(productId) {
-    alert(`Product with ID ${productId} added to cart! (Simulated)`);
+    const selectedProduct = products.find(product => product.id === productId);
+    if (selectedProduct) {
+      cart.push(selectedProduct);
+      alert(`${selectedProduct.name} added to cart!`);
+    }
   }
   
-  // Function to handle "View Project" button click
-  function viewEcommerceProject() {
-    alert('Redirecting to the E-commerce project page! (Simulated)');
+  // Function to view the cart
+  function viewCart() {
+    if (cart.length === 0) {
+      alert('Your cart is empty.');
+    } else {
+      const cartItems = cart.map(item => `${item.name} - ${item.price.toFixed(2)} USD`).join('\n');
+      alert(`Your Cart:\n${cartItems}\nTotal: ${calculateTotal(cart)} USD`);
+    }
+  }
+  
+  // Function to calculate the total price of items in the cart
+  function calculateTotal(cart) {
+    return cart.reduce((total, item) => total + item.price, 0).toFixed(2);
+  }
+  
+  // Function to go back to the main page
+  function goBack() {
+    window.location.href = 'index.html';
   }
   
