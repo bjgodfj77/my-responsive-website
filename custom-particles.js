ab3721092d0ab83b13f30acdb01e0021534f4047
bjgodfj77 <150914353+bjgodfj77@users.noreply.github.com>
2023-11-16 17:33:28 +0000

    Changed the way the web page works. After break 0

--- custom-particles.js
+++ custom-particles.js
@@ -1,106 +1,68 @@
-particlesJS("particles-js", {
-    particles: {
-      number: {
-        value: 80,
-        density: {
-          enable: true,
-          value_area: 800,
-        },
-      },
-      color: {
-        value: "#2e3fff", // Particle color
-      },
-      shape: {
-        type: "circle",
-        stroke: {
-          width: 0,
-          color: "#000000",
-        },
-        polygon: {
-          nb_sides: 5,
-        },
-      },
-      opacity: {
-        value: 0.5,
-        random: false,
-        anim: {
-          enable: false,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false,
-        },
-      },
-      size: {
-        value: 3,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 40,
-          size_min: 0.1,
-          sync: false,
-        },
-      },
-      line_linked: {
-        enable: true,
-        distance: 150,
-        color: "#2e3fff", // Line color
-        opacity: 0.4,
-        width: 1,
-      },
-      move: {
-        enable: true,
-        speed: 6,
-        direction: "none",
-        random: false,
-        straight: false,
-        out_mode: "out",
-        bounce: false,
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200,
-        },
-      },
-    },
-    interactivity: {
-      detect_on: "canvas",
-      events: {
-        onhover: {
-          enable: true,
-          mode: "repulse",
-        },
-        onclick: {
-          enable: true,
-          mode: "push",
-        },
-        resize: true,
-      },
-      modes: {
-        grab: {
-          distance: 400,
-          line_linked: {
-            opacity: 1,
+// custom-particles.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  particlesJS('particles-js', {
+      particles: {
+          // Your particle settings
+          number: {
+              value: 80,
+              density: {
+                  enable: true,
+                  value_area: 800
+              }
           },
-        },
-        bubble: {
-          distance: 400,
-          size: 40,
-          duration: 2,
-          opacity: 8,
-          speed: 3,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
-        push: {
-          particles_nb: 4,
-        },
-        remove: {
-          particles_nb: 2,
-        },
+          size: {
+              value: 3
+          },
+          color: {
+              value: "#2e3fff"
+          },
+          line_linked: {
+              enable: true,
+              distance: 150,
+              color: "#2e3fff",
+              opacity: 0.4
+          },
+          move: {
+              enable: true,
+              speed: 6,
+              direction: "none",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              bounce: false
+          }
       },
-    },
-    retina_detect: true,
+      interactivity: {
+          detect_on: "canvas",
+          events: {
+              onhover: {
+                  enable: false,
+                  mode: "grab"
+              }
+          }
+      }
   });
-  
+
+  // Animate project sections on scroll
+  const projectSections = document.querySelectorAll('.project');
+  window.addEventListener('scroll', animateProjects);
+
+  function animateProjects() {
+      projectSections.forEach((section) => {
+          if (isElementInViewport(section)) {
+              section.classList.add('animated');
+          }
+      });
+  }
+
+  function isElementInViewport(element) {
+      const rect = element.getBoundingClientRect();
+      return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+  }
+});
