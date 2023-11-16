bc61cbb824f97d7ddea4a7e398be9cf7842022aa
bjgodfj77 <150914353+bjgodfj77@users.noreply.github.com>
2023-11-16 15:16:33 +0000

    Changed the color of the task.

--- custom_web_application.js
+++ custom_web_application.js
@@ -1,18 +1,32 @@
 // custom_web_application.js
 
 async function addTask() {
-    // Your existing addTask function logic
-  }
-  
-  async function fetchTasks() {
-    // Your existing fetchTasks function logic
-  }
-  
-  // Function to go back to the main page
-  function goBack() {
-    window.location.href = '/';
-  }
-  
-  // Fetch tasks on page load
-  document.addEventListener('DOMContentLoaded', fetchTasks);
-  
+  // Your existing addTask function logic
+}
+
+async function fetchTasks() {
+  // Your existing fetchTasks function logic
+}
+
+// Function to go back to the main page
+function goBack() {
+  window.location.href = '/';
+}
+
+// Fetch tasks on page load
+document.addEventListener('DOMContentLoaded', fetchTasks);
+
+// Set the color to #2e3fff
+const color = '#2e3fff';
+
+// Example: Update the color of an element with the class "your-element-class"
+const element = document.querySelector('.your-element-class');
+if (element) {
+  element.style.color = color;
+}
+
+// Example: Update the background color of an element with the class "your-other-element-class"
+const otherElement = document.querySelector('.your-other-element-class');
+if (otherElement) {
+  otherElement.style.backgroundColor = color;
+}
